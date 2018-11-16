e9d15c34ffbda28eeb58c3e065cd104e23ef7668
Aditya Kanodia <kanodia.aditya@in.bosch.com>
2018-11-16 15:44:05 +0530

    Creation of project

---
+++ UdemySpringHibernat/src/main/java/com/example/practice/UdemySpringHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.example.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringHibernateApplication.class, args);
+	}
+}
