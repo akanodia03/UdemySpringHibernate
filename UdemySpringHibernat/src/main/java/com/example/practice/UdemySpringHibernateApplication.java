7fc709ab65a4d2c79976936eeea13fe8416606ec
Aditya Kanodia <kanodia.aditya@in.bosch.com>
2018-11-19 16:26:31 +0530

    First Commit Section 2

--- UdemySpringHibernat/src/main/java/com/example/practice/UdemySpringHibernateApplication.java
+++ UdemySpringHibernat/src/main/java/com/example/practice/UdemySpringHibernateApplication.java
@@ -1,12 +1,19 @@
 package com.example.practice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class UdemySpringHibernateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UdemySpringHibernateApplication.class, args);
+		ApplicationContext applicationContext= SpringApplication.run(UdemySpringHibernateApplication.class, args);
+		
+		for(String string: applicationContext.getBeanDefinitionNames()) {
+			
+			System.out.println(string);
+		}
 	}
 }
